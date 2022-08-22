a4701ddb9861b2a09fa18b8a9fb491b7e6d1a15d
erfanzar <erfanzare82@yahoo.com>
2022-08-22 18:47:36 +0430

    bugs

--- src/App.js
+++ src/App.js
@@ -2,49 +2,42 @@ import './App.css';
 import Webcam from 'react-webcam';
 import React, {useRef, useState, useEffect} from 'react';
 import * as handPoseDetection from '@tensorflow-models/hand-pose-detection'
-import * as face from '@tensorflow-models/blazeface';
-import * as tf from '@tensorflow/tfjs';
-import {log, math} from "@tensorflow/tfjs";
-import {type} from "@testing-library/user-event/dist/type";
-import {useMediaQuery} from "react-responsive";
+import * as faceDetection from '@tensorflow-models/face-detection'
+import * as tf from '@tensorflow/tfjs'
 
 
 function App() {
 
-
     const iml = ['mid', 'to bottom left', 'to bottom right', 'top']
-
+    let v = null
     const WebcamRef = useRef(null);
     const [test, setTest] = useState(0)
     const [color, setColor] = useState('white');
     const [started, setStarted] = useState(false);
     const [w, sw] = useState(0);
     const [h, sh] = useState(0);
-    const [distance, setDistance] = useState(150);
-    const [x0, sx0] = useState(0);
-    const [y0, sy0] = useState(0);
-    const [x1, sx1] = useState(0);
-    const [y1, sy1] = useState(0);
+    const [distance, setDistance] = useState(70);
+
     const [fx, fsx] = useState(0);
     const [fy, fsy] = useState(0);
     const [modelStatus, setModelStatus] = useState(false);
     const [fontSize, setFontSize] = useState(12);
     const [status, setStatus] = useState('Start');
     const [indexImages, setIndexImages] = useState(0)
-    const [passTime, setPassTime] = useState(null)
-    const model = handPoseDetection.SupportedModels.MediaPipeHands;
     let route = 0;
-    let route_time = 0;
+    let Allowed_size = 430
 
-    let df = null;
     let t = 0;
     let cpa = 0
+
+    const fully_width = window.innerWidth
     const run = async (detector, model_face) => {
         if (
             typeof WebcamRef.current !== "undefined" &&
             WebcamRef.current !== null &&
             typeof WebcamRef.current.video !== "undefined"
         ) {
+
             const hands = await detector.estimateHands(WebcamRef.current.video)
 
             if (hands?.[0]?.score !== undefined) {
@@ -65,8 +58,6 @@ function App() {
                     setStarted(true)
                     cpa = 1
                 }
-
-                // console.log(`should go from : ${hs + 10} current : ${lmy} end :${hs + (window.innerWidth / 9)}`)
                 if (-500 < lmx &&
                     lmx < ws + 20 + (window.innerWidth / 9) &&
                     hs + 10 < lmy &&
@@ -96,20 +87,24 @@ function App() {
                     )
                 }
             }
-            const faces = await model_face.estimateFaces(WebcamRef.current.video)
-            if (faces[0]?.probability !== undefined) {
-                sx0(Math.abs(((faces[0].topLeft[0]) / 640) * window.innerWidth))
-                sy0(Math.abs(((faces[0].topLeft[1]) / 640) * window.innerHeight))
+            const video = WebcamRef.current.video
+            const faces = await model_face.estimateFaces(video, false)
 
-                sx1(Math.abs(((faces[0].bottomRight[0]) / 640) * window.innerWidth))
-                sy1(Math.abs(((faces[0].bottomRight[1]) / 640) * window.innerHeight))
+            if (faces.length > 0) {
+                sw(faces[0].box.width)
+                sh(faces[0].box.height)
 
-                sw(Math.abs(((faces[0].topLeft[0] - faces[0].bottomRight[0]))))
-                sh(Math.abs(((faces[0].topLeft[1] - faces[0].bottomRight[1]))))
+                const fw = WebcamRef.current.video.videoWidth
+                let wa = (faces[0].box.width / fw) * 640
+                console.log(faces)
+                console.log(WebcamRef.current.video.videoWidth)
 
-                if (cpa === 1) {
-                    setDistance(Math.floor(((640 - w) / (w / 640)) / 10))
+                if (wa * 10 < 15) {
+                    setDistance("Nan");
+                } else {
+                    setDistance(Math.floor(((wa * 10) * 2.6) * 1.5));
                 }
+
 
             }
         }
@@ -119,15 +114,21 @@ function App() {
     const LoadModel = async () => {
         if (route === 0) {
             route = 1
+
             console.log('Loading Face Model ...')
-            const model_face = await face.load(1, 640, 640, 0.4, 0.4)
+            const faceModel = faceDetection.SupportedModels.MediaPipeFaceDetector;
+            const model_face = await faceDetection.createDetector(faceModel, {
+                runtime: 'tfjs',
+                maxFaces: 1,
+                modelType: 'full'
+            })
             console.log('Face Model Loaded Successfully')
 
             const model = handPoseDetection.SupportedModels.MediaPipeHands;
 
             const detectorConfig = {
                 runtime: 'tfjs',
-                modelType: 'lite'
+                modelType: 'full'
             }
             console.log('Loading Model ...')
             const detector = await handPoseDetection.createDetector(model, detectorConfig)
@@ -137,10 +138,12 @@ function App() {
             setModelStatus(true)
             setInterval(() => {
                 run(detector, model_face)
-                setPassTime(t)
+                // setPassTime(t)
             }, 100)
         }
     }
+
+
     const changeImage = () => {
 
         setInterval(() => {
@@ -178,8 +181,6 @@ function App() {
 
                 <Webcam
                     ref={WebcamRef}
-                    height={640}
-                    width={640}
                     mirrored={true}
                     style={{
                         height: '0px',
@@ -194,8 +195,8 @@ function App() {
                             src={'./assets/images/Dg.jpg'} alt={'test'} style={
                             {
                                 display: "flex",
-                                width: `${distance}px`,
-                                height: `${distance}px`,
+                                width: `${50}px`,
+                                height: `${50}px`,
                                 alignItems: "center",
                                 margin: "auto"
                             }
@@ -225,20 +226,6 @@ function App() {
 
                 </div>
 
-
-                {/*<div*/}
-                {/*    style={{*/}
-                {/*        position: 'absolute',*/}
-                {/*        position: 'absolute',*/}
-                {/*        height: `${h}px`,*/}
-                {/*        width: `${w}px`,*/}
-                {/*        top: `${y0}px`,*/}
-                {/*        right: `${x0}px`,*/}
-                {/*        borderColor: 'cyan',*/}
-                {/*        backgroundColor:'cyan',*/}
-                {/*        // borderRadius: '50%',*/}
-                {/*        transition: "all 0.3s"*/}
-                {/*    }}/>*/}
 
                 <div
                     style={{
@@ -438,9 +425,9 @@ function App() {
                                     fontSize: `${fontSize}px`,
                                 }
                             }>
-                            Your current Distance is : {Math.floor(((640 - w) / (w / 640)) / 10)} cm
+                            Your current Distance is : {distance} cm
                         </p>
-                        {Math.floor((350 - (w / 3)) / 1.6) > 60 ?
+                        {Math.floor((640 * w) / 5000) > 60 ?
                             <p
                                 style={
                                     {
@@ -455,45 +442,30 @@ function App() {
                                 style={
                                     {
                                         position: 'absolute',
-                                        top: '40px',
+                                        top: '30px',
                                         left: '15px',
                                         fontSize: `${fontSize}px`,
                                     }
                                 }>
                                 You Can't Start the test Minimum Distance required is 60 cm
                             </p>
+
                         }
+                        <p
+                            style={
+                                {
+                                    position: 'absolute',
+                                    top: '70px',
+                                    left: '15px',
+                                    fontSize: `${fontSize}px`,
+                                }
+                            }>
+                            Width f Person {w}
+                        </p>
+
                     </div> : <div/>
 
                 }
-                {/*{started === true ?*/}
-                {/*    <div*/}
-                {/*        style={{*/}
-                {/*            height: `${window.innerHeight / 2}px`,*/}
-                {/*            width: `${window.innerWidth / 8}px`,*/}
-                {/*            backgroundColor: 'cyan',*/}
-                {/*            borderRadius: '20px',*/}
-                {/*            position: 'absolute',*/}
-
-                {/*            right: `10px`*/}
-                {/*        }}*/}
-                {/*    >*/}
-                {/*        <p*/}
-                {/*            style={{*/}
-                {/*                // position:'relative',*/}
-                {/*                fontSize: `${fontSize}px`,*/}
-                {/*                padding: '10px',*/}
-                {/*                color: 'black'*/}
-                {/*            }}>*/}
-                {/*            first of all you have to get away from the camera for al least 60 cm and from the screen and*/}
-                {/*            then close your one of your eyes with your hands*/}
-                {/*            repeat this process for both of your eyes for the point of this test is that you shouldn't*/}
-                {/*            see the lines with different thickness and you shouldn't also see different shape for the*/}
-                {/*            circle*/}
-                {/*        </p>*/}
-
-                {/*    </div> : <div></div>*/}
-                {/*}*/}
             </div>
         )
     } else {
